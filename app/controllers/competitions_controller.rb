702b071a39841a43c996fd7c2cd1ebff37c15384
Nathan Malishev <nathan.malishev@gmail.com>
2016-06-03 08:26:25 +1000

    Added functionality to create competions

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,4 +1,25 @@
 class CompetitionsController < ApplicationController
   def index
+    @competitions = Competition.all
   end
+
+  def new
+    @competition = Competition.new
+  end
+
+  def create
+    @competition = Competition.new(competition_params)
+    if @competition.save
+      redirect_to competitions_path
+    else
+      render 'new'
+    end
+  end
+
+
+
+  private
+    def competition_params
+      params.require(:competition).permit(:title,:description)
+    end
 end
